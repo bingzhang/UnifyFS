92065e294f14985aa406a923852d1918f250b8ba
Michael J. Brim <brimmj@ornl.gov>
2020-10-29 15:46:02 -0400

    fixup - gcc8 compile

--- common/src/unifyfs_log.h
+++ common/src/unifyfs_log.h
@@ -78,7 +78,7 @@ void unifyfs_set_log_level(unifyfs_log_level_t lvl);
         } \
         const char* srcfile = __FILE__ + unifyfs_log_source_base_len; \
         time_t log_time = time(NULL); \
-        char message[1024]; \
+        char message[2048]; \
         snprintf(message, sizeof(message), __VA_ARGS__); \
         unifyfs_log_print(log_time, srcfile, __LINE__, __func__, message); \
     }
